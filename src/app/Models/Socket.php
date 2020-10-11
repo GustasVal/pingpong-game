05b82e13597e03a0800e322620de2bf476f7283d
GustasValdavicius <gustas.valdavicius@hostinger.com>
2020-10-11 14:22:02 +0300

    WebSockets Ratchet

---
+++ src/app/Models/Socket.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use SplObjectStorage;
+
+class Socket implements MessageComponentInterface
+{
+    /**
+     * @var SplObjectStorage
+     */
+    private $clients;
+
+    public function __construct()
+    {
+        $this->clients = new SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+
+        // Store the new connection in $this->clients
+        $this->clients->attach($conn);
+
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+
+        foreach ( $this->clients as $client ) {
+
+            if ( $from->resourceId == $client->resourceId ) {
+                continue;
+            }
+
+            $client->send( "Client $from->resourceId said $msg" );
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+    }
+}
